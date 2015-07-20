f8e5965eef2c45a6f96279f3df5648c403720be5
Gilberto <gilbertomtz672@gmail.com>
2015-07-19 22:10:25 -0500

    Initial commit

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
-function find(UniversityName) {
-    var apiUrl="http://localhost:1337/api/University";
+function find(UniversityName, fn) {
+    var apiUrl="http://localhost:3000/api/University";
     var id = UniversityName;
     $.getJSON(apiUrl + '/' + id)
         .done(function (data) {
-            console.log(data);
-            return data;
+            //console.log("find method "+data);
+            fn(data);
         })
         .fail(function (jqXHR, textStatus, err) {
             console.log("error");
@@ -12,6 +12,20 @@ function find(UniversityName) {
 }
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    find(request.university);
-    sendResponse({farewell: "goodbye"});
+    //find(request.university,function(data){
+    //    sendResponse({res: data});
+    //    console.log(data);
+    //});
+      var apiUrl="http://localhost:3000/api/University";
+      var id = request.university;
+      $.getJSON(apiUrl + '/' + id)
+          .done(function (data) {
+              console.log(data);
+              sendResponse(data);
+          })
+          .fail(function (jqXHR, textStatus, err) {
+              console.log("error");
+          });
+
+
   });
--- popup.js
+++ popup.js
@@ -40,7 +40,9 @@ function getFromAddress(){
                  console.log(headers[i].value);
                  var match=regex.exec(headers[i].value);
                  console.log(match[1]);
-                 chrome.runtime.sendMessage({university: match[1]});
+                 chrome.runtime.sendMessage({university: match[1]},function(response){
+                     console.log(response.res);
+                 });
              }
                  
          }
