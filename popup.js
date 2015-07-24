3ab52b1e694afbe63e5dc510a2bbfdd5ce4b0215
Gilberto <gilbertomtz672@gmail.com>
2015-07-23 23:41:13 -0500

    Added SAT scores and small description based on size of the school and where it's located. Some CSS to make the popup window larger

--- popup.js
+++ popup.js
@@ -44,6 +44,7 @@ function getFromAddress(){
                  chrome.extension.getBackgroundPage().find(match[1],function(r){
                      displayInfo(r);
                      console.log(r);
+
                  });
              }
                  
@@ -52,9 +53,27 @@ function getFromAddress(){
 }
 function displayInfo(jsonObj){
     document.getElementById('name').textContent = "Name: "+ jsonObj[0].name;
-    document.getElementById('city').textContent = "City: " +jsonObj[0].city;
-    document.getElementById('state').textContent = "State: "+jsonObj[0].state;
+    document.getElementById('location').textContent = "Location: " +jsonObj[0].city+","+jsonObj[0].state;
+    document.getElementById('description').textContent=formatDescription(jsonObj);
     document.getElementById('admissionRate').textContent = "Admission Rate: "+jsonObj[0].percentAdmitted.total+"%";
+    document.getElementById('satm').textContent="SAT Math: "+jsonObj[0].sat.math25thPercentileScore+"-"+jsonObj[0].sat.math75thPercentileScore;
+    document.getElementById('satr').textContent="SAT Reading: "+jsonObj[0].sat.reading25thPercentileScore+"-"+jsonObj[0].sat.reading75thPercentileScore;
+    document.getElementById('satw').textContent="SAT Writing: "+jsonObj[0].sat.writing25thPercentileScore+"-"+jsonObj[0].sat.writing75thPercentileScore;
 
 
+}
+function formatDescription(j){
+    //A large 4 year college located in a large city setting
+    var description="";
+    //Large four-year, primarily nonresidential
+    var bit= j[0].carnegieClassification.sizeSetting;
+    var size=bit.split(",")[0];
+    size=size.toLowerCase();
+    //"City: Large"
+    var tidbit=j[0].urbanization;
+    var split=tidbit.split(":");
+    var urbanization=split[1]+ " "+ split[0];
+    urbanization=urbanization.toLowerCase();
+    description="A "+size+" college located in a "+urbanization+" setting";
+    return description;
 }
