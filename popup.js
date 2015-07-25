7b12a1f39ae415e409d477ccbe36afd55b3172d1
Gilberto <gilbertomtz672@gmail.com>
2015-07-24 19:45:55 -0500

    Added an options page. Copy pasted from the google dev website. The extension now splits the from address domain if it contains a period ("."). This helps match more colleges.

--- popup.js
+++ popup.js
@@ -34,31 +34,42 @@ function getFromAddress(){
     });
     request.execute(function(message){
         var headers=message.payload.headers;
-        var regex=new RegExp('@(.*).edu>');
+        var regex=new RegExp('<.*@(.*).edu>');
         for(var i=0;i<headers.length;i++){
              if (headers[i].name == 'From'){
                  //console.log(headers[i].value);
                  var match=regex.exec(headers[i].value);
                  console.log(match);
                  //console.log(match[1]);
-                 chrome.extension.getBackgroundPage().find(match[1],function(r){
-                     displayInfo(r);
-                     console.log(r);
+                 if(match[1].indexOf(".")!=-1){
+                     var temp=match[1].split("\.")[1];
+                     console.log(temp);
+                     chrome.extension.getBackgroundPage().find(temp,function(r){
+                         displayInfo(r);
+                         console.log(r);
 
-                 });
+                     });
+                 }else{
+                     chrome.extension.getBackgroundPage().find(match[1],function(r){
+                         displayInfo(r);
+                         console.log(r);
+
+                     });
+                 }
+
              }
                  
          }
     });
 }
 function displayInfo(jsonObj){
-    document.getElementById('name').textContent = "Name: "+ jsonObj[0].name;
-    document.getElementById('location').textContent = "Location: " +jsonObj[0].city+","+jsonObj[0].state;
+    document.getElementById('name').textContent = "Name: "+ jsonObj.name;
+    document.getElementById('location').textContent = "Location: " +jsonObj.city+","+jsonObj.state;
     document.getElementById('description').textContent=formatDescription(jsonObj);
-    document.getElementById('admissionRate').textContent = "Admission Rate: "+jsonObj[0].percentAdmitted.total+"%";
-    document.getElementById('satm').textContent="SAT Math: "+jsonObj[0].sat.math25thPercentileScore+"-"+jsonObj[0].sat.math75thPercentileScore;
-    document.getElementById('satr').textContent="SAT Reading: "+jsonObj[0].sat.reading25thPercentileScore+"-"+jsonObj[0].sat.reading75thPercentileScore;
-    document.getElementById('satw').textContent="SAT Writing: "+jsonObj[0].sat.writing25thPercentileScore+"-"+jsonObj[0].sat.writing75thPercentileScore;
+    document.getElementById('admissionRate').textContent = "Admission Rate: "+jsonObj.percentAdmitted.total+"%";
+    document.getElementById('satm').textContent="SAT Math: "+jsonObj.sat.math25thPercentileScore+"-"+jsonObj.sat.math75thPercentileScore;
+    document.getElementById('satr').textContent="SAT Reading: "+jsonObj.sat.reading25thPercentileScore+"-"+jsonObj.sat.reading75thPercentileScore;
+    document.getElementById('satw').textContent="SAT Writing: "+jsonObj.sat.writing25thPercentileScore+"-"+jsonObj.sat.writing75thPercentileScore;
 
 
 }
@@ -66,11 +77,11 @@ function formatDescription(j){
     //A large 4 year college located in a large city setting
     var description="";
     //Large four-year, primarily nonresidential
-    var bit= j[0].carnegieClassification.sizeSetting;
+    var bit= j.carnegieClassification.sizeSetting;
     var size=bit.split(",")[0];
     size=size.toLowerCase();
     //"City: Large"
-    var tidbit=j[0].urbanization;
+    var tidbit=j.urbanization;
     var split=tidbit.split(":");
     var urbanization=split[1]+ " "+ split[0];
     urbanization=urbanization.toLowerCase();
